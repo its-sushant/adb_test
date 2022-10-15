4f6fd42290502bbbeea55ff18b4cabe584978717
Sushant <sushantmishra02102002@gmail.com>
2022-10-15 13:56:24 +0530

    improve django code format

--- src/rest/rest/todoserializer.py
+++
@@ -1,4 +0,0 @@
-from rest_framework import serializers
-
-class TodoSerializer(serializers.Serializer):
-    todo = serializers.CharField(max_length=200)
--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -5,13 +5,12 @@ from rest_framework.response import Response
 from rest_framework import status
 import json, logging, os
 from pymongo import MongoClient
-from .todoserializer import TodoSerializer
 from .serializer import Todo
 
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db1 = MongoClient(mongo_uri)
 db = db1['test_db']
-collection = db.mydatabase
+collection = db.mynewdatabase
 
 class TodoListView(APIView):
 
@@ -20,14 +19,17 @@ class TodoListView(APIView):
         todos = collection.find()
 
         for todo in todos:
-            #serializer = TodoSerializer(data=todo)
-            #if serializer.is_valid():
-            data = Todo(todo=todo.get('todo'))
+            try:
+                data = Todo(todo=todo.get('todo'))
+            except KeyError:
+                print("key not found")
+
             try:
                 temp = json.dumps(data, default=data.encode_todo)
                 result.append(eval(temp))
-            except ValueError:
-                pass
+            except TypeError:
+                print("Not a valid serializer")
+
         return Response(result, status=status.HTTP_200_OK)
         
     def post(self, request):
@@ -40,7 +42,6 @@ class TodoListView(APIView):
             return Response('ok!', status=status.HTTP_200_OK)
         except NameError:
             print("not a valid serialize!")
-
         
         return Response('error!', status=status.HTTP_400_BAD_REQUEST)
 
